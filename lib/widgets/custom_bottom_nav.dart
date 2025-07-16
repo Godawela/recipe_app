8cf3dcea6bc5ab58fea5eabdc6f4b967fd80be8f
Godawela <godawelaus.21@uom.lk>
2025-07-16 16:48:47 +0530

    init

---
+++ lib/widgets/custom_bottom_nav.dart
@@ -0,0 +1,61 @@
+import 'package:flutter/material.dart';
+
+class CustomBottomNav extends StatelessWidget {
+  final int currentIndex;
+  final Function(int) onTap;
+
+  const CustomBottomNav({
+    Key? key,
+    required this.currentIndex,
+    required this.onTap,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final items = [
+      Icons.home,
+      Icons.search,
+      Icons.bookmark_outline,
+      Icons.person_outline,
+    ];
+
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 12),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.1),
+            blurRadius: 10,
+            offset: Offset(0, -5),
+          ),
+        ],
+      ),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: items.asMap().entries.map((entry) {
+          int index = entry.key;
+          IconData icon = entry.value;
+          bool isActive = index == currentIndex;
+          
+          return GestureDetector(
+            onTap: () => onTap(index),
+            child: Container(
+              padding: EdgeInsets.all(12),
+              decoration: BoxDecoration(
+                color: isActive ? Colors.orange[600] : Colors.transparent,
+                borderRadius: BorderRadius.circular(12),
+              ),
+              child: Icon(
+                icon,
+                color: isActive ? Colors.white : Colors.grey[400],
+                size: 24,
+              ),
+            ),
+          );
+        }).toList(),
+      ),
+    );
+  }
+}
+
